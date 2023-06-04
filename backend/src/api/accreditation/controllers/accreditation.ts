9b78e2eb607db201cf4671d16b9d408e82018da3
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-04 09:37:48 +0800

    feat: accreditation
    Added a new collection type that allows record keeping of accreditation.
    With direct relation to academic programme, this allows for easy
    retrieval of data for analytics dashboard

---
+++ backend/src/api/accreditation/controllers/accreditation.ts
@@ -0,0 +1,7 @@
+/**
+ * accreditation controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::accreditation.accreditation');
---
+++ backend/src/api/accreditation/routes/accreditation.ts
@@ -0,0 +1,7 @@
+/**
+ * accreditation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::accreditation.accreditation');
