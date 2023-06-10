42d821335f1d2929c877415edee7e32de46e0757
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-10 15:36:18 +0800

    fix: cannot send test email

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,17 +1,19 @@
 export default ({ env }) => ({
   email: {
-    provider: env("EMAIL_PROVIDER"),
-    providerOptions: {
-      host: env("EMAIL_SMTP_HOST", "smtp.example.com"),
-      port: env("EMAIL_SMTP_PORT", 587),
-      auth: {
-        user: env("EMAIL_SMTP_USER"),
-        pass: env("EMAIL_SMTP_PASS"),
+    config: {
+      provider: env("EMAIL_PROVIDER"),
+      providerOptions: {
+        host: env("EMAIL_SMTP_HOST", "smtp.example.com"),
+        port: env("EMAIL_SMTP_PORT", 587),
+        auth: {
+          user: env("EMAIL_SMTP_USER"),
+          pass: env("EMAIL_SMTP_PASS"),
+        },
       },
-    },
-    settings: {
-      defaultFrom: env("EMAIL_ADDRESS_FROM"),
-      defaultReplyTo: env("EMAIL_ADDRESS_REPLY"),
+      settings: {
+        defaultFrom: env("EMAIL_ADDRESS_FROM"),
+        defaultReplyTo: env("EMAIL_ADDRESS_REPLY"),
+      },
     },
   },
 });
